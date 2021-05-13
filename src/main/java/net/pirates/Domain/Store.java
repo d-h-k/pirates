ba7dec297f4d253d25eec4a5fd7c36bc06326fb3
dong <ultraraptor@naver.com>
2021-05-13 13:53:21 +0900

    feat : 점포 data 제공 API 제공을 위한 도메인 작성

---
+++ src/main/java/net/pirates/Domain/Store.java
@@ -0,0 +1,12 @@
+package net.pirates.Domain;
+
+//@Entity
+public class Store {
+    private String name;
+    private String owner;
+    private String description;
+    private Integer level;
+    private String address;
+    private String phone;
+    private String businessTimes;// 수정 필요 SET?? List?
+}
