1dbf532e479b40705470c7a7c44315ea51073ff6
DongHunKim <ultraraptor@naver.com>
2021-05-14 13:46:28 +0900

    fix untracked files

--- src/main/java/net/pirates/Domain/Store.java
+++ src/main/java/net/pirates/Domain/Store.java
@@ -1,7 +1,16 @@
 package net.pirates.Domain;
 
-//@Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Store {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String name;
     private String owner;
     private String description;
--- src/main/java/net/pirates/Repository/StoreRepository.java
+++ src/main/java/net/pirates/Repository/StoreRepository.java
@@ -2,7 +2,9 @@ package net.pirates.Repository;
 
 import net.pirates.Domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface StoreRepository extends JpaRepository<Long, Store> {
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
 
 }
