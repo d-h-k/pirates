4a318f30f5f30dada7b9f9029dd172fa2def82c3
DongHunKim <ultraraptor@naver.com>
2021-05-14 15:52:12 +0900

    DTO의 getter/setter 설정

--- src/main/java/net/pirates/DTOs/AddStoreDTO.java
+++ src/main/java/net/pirates/DTOs/AddStoreDTO.java
@@ -14,4 +14,60 @@ public class AddStoreDTO {
     private String phone;
     private List<BusinessTime> businessTimes;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<BusinessTime> getBusinessTimes() {
+        return businessTimes;
+    }
+
+    public void setBusinessTimes(List<BusinessTime> businessTimes) {
+        this.businessTimes = businessTimes;
+    }
 }
